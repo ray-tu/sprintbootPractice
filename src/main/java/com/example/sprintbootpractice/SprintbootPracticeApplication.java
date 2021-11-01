33e5651796428de637b694aeb9d200e68b0349d4
Ray Tu <ray.tu@icloud.com>
2021-11-01 17:13:38 -0400

    SprintBootPractice

---
+++ src/main/java/com/example/sprintbootpractice/SprintbootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintbootPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprintbootpractice/SprintbootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
